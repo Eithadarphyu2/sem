195f3f96aed324d1296460e1be66137b0ff72581
Eithadarphyu2 <40840476@live.napier.ac.uk>
2026-09-17 14:30:15 +0630

    Extract database connection methods

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,12 +2,24 @@ package com.napier.sem;
 
 import java.sql.*;
 
+/**
+ * Main application class.
+ */
 public class App
 {
-    public static void main(String[] args)
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
     {
         try
         {
+            // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch (ClassNotFoundException e)
@@ -16,9 +28,7 @@ public class App
             System.exit(-1);
         }
 
-        Connection con = null;
-
-        int retries = 100;
+        int retries = 10;
 
         for (int i = 0; i < retries; ++i)
         {
@@ -26,49 +36,66 @@ public class App
 
             try
             {
+                // Wait a bit for db to start
                 Thread.sleep(30000);
 
+                // Connect to database
                 con = DriverManager.getConnection(
                         "jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false",
                         "root",
-                        "example"
-                );
+                        "example");
 
                 System.out.println("Successfully connected");
 
-                Thread.sleep(10000);
-
+                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
             {
                 System.out.println(
                         "Failed to connect to database attempt "
-                                + Integer.toString(i)
-                );
+                                + Integer.toString(i));
 
                 System.out.println(sqle.getMessage());
             }
             catch (InterruptedException ie)
             {
-                System.out.println(
-                        "Thread interrupted? Should not happen."
-                );
+                System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
             {
+                // Close connection
                 con.close();
             }
             catch (Exception e)
             {
-                System.out.println(
-                        "Error closing connection to database"
-                );
+                System.out.println("Error closing connection to database");
             }
         }
+    }
+
+    /**
+     * Main application method.
+     */
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
